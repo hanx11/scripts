e7d4c1cd6d948f004c4414167ae4ac155b6c945d
hanx11 <hanx11@163.com>
2016-01-21 11:34:49 +0800

    test pymysql script

---
+++ pymysql-test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                            user='root',
+                            password='hanfeng',
+                            db='mysql',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    # with connection.cursor() as cursor:
+        # Create a new record
+        # sql = '''create table user_table (
+        #             user_id INT NOT NULL AUTO_INCREMENT,
+        #             user_name VARCHAR(100) NOT NULL,
+        #             user_age INT,
+        #             submission_date DATE,
+        #             PRIMARY KEY (user_id) 
+        #         );
+        #       '''
+        # cursor.execute(sql)
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    # connection.commit()
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "select version(), current_date; select now();"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+        print(type(result))
+except Exception as e:
+    raise e
+finally:
+    connection.close()
